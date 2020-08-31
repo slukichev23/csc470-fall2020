dbe3d2a4b57de723ce7380e58937e2da617958dc
Semyon Lukichev <semyon.lukichev@gmail.com>
2020-08-31 16:23:52 -0400

    full project

---
+++ exercises/RollaBall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    // Update is called once per frame
+    // 2 ways of affection transform: translate and rotate
+    // 2 initial params: vector3 (simplest) variable or 3 float values
+    void Update()
+    {
+        // multiply by delta time to make it smooth and framerate independent
+        // dynamically changes value of time based upon length of a frame
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
