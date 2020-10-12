ef40ede2e139e4598cb91a86c3fe70207a13ef81
Semyon Lukichev <semyon.lukichev@gmail.com>
2020-10-12 16:07:40 -0400

    added basic moving platforms

---
+++ exercises/game04/Assets/Scripts/PlatformVerticalMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformVerticalMovement : MonoBehaviour
+{
+    float movementSpeed = 0.001f; // This variable changes
+	float baseMovementSpeed = 0.001f;
+	float movementSpeedSlowed = 0.00075f;
+	float moveTimer = 2.5f;
+	float moveRate = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// Timer 
+    	moveTimer -= Time.deltaTime;
+    	// Moves platform
+    	transform.Translate(Vector3.up * movementSpeed, Space.Self);
+    	// Slow down a little bit before changing direction
+    	if (moveTimer < 0.5f && moveTimer > 0f){
+    		movementSpeed = (movementSpeed > 0) ? movementSpeedSlowed : movementSpeedSlowed * -1;
+    	} else {
+    		movementSpeed = (movementSpeed > 0) ? baseMovementSpeed : baseMovementSpeed * -1;
+    	}
+    	// Changes direction after timer
+        if (moveTimer <= 0){
+        	movementSpeed *= -1;
+        	moveTimer = moveRate;
+        }
+    }
+}
