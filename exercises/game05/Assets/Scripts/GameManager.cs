ec5ba321071490dd32e7fbb63b982a4ce5a873a8
Semyon Lukichev <semyon.lukichev@gmail.com>
2020-11-03 15:12:17 -0500

    added power plant as a placeable building within UI of the main base

---
+++ exercises/game05/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject MainBase;
+    public GameObject PowerPlantPrefab;
+    MainBaseScript mb;
+
+    void Start()
+    {
+
+        mb = MainBase.GetComponent<MainBaseScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    	// Updates color of buildings to tell player if they can build them or not (red for no white for yes)
+    	if (mb.BuildModePowerPlant == false){
+    		if (!(mb.money >= 100 && mb.metals >= 10))
+    		{
+    			mb.PowerPlantBtn.image.color = Color.red;
+    		}
+    	}
+
+
+        // Handles placing down buildings
+        if (mb.BuildModePowerPlant){
+        	// When player clicks on ground, that's where prefab will be created
+        	if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit)) {
+                    Debug.Log("Power Plant built");
+                    mb.BuildModePowerPlant = false;
+                    Instantiate(PowerPlantPrefab, hit.point, Quaternion.identity);
+                    mb.updateStatsUI();
+                    mb.PowerPlantBtn.image.color = Color.white;
+                }
+            }
+        }
+    }
+}
--- exercises/game05/Assets/Scripts/MainBaseScript.cs
+++ exercises/game05/Assets/Scripts/MainBaseScript.cs
@@ -6,20 +6,35 @@ using UnityEngine.EventSystems;
 
 public class MainBaseScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     public Text BaseSelectedText;
+    public Text moneyText;
+    public Text powerText;
+    public Text metalsText;
+    public Text techlvlText;
 	public bool selected = false;
     public GameObject BaseCanvas;
+    public bool BuildModePowerPlant = false;
+    public Button PowerPlantBtn;
+
+    // Resource Stats
+    public int money = 500;
+    public int power = 0;
+    public int metals = 50;
+    public int techlvl = 0;
+
 
     void Start()
     {
         BaseCanvas.SetActive(false);
-        BaseSelectedText.text = "Base Selected";
+        updateStatsUI();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         // ALL OF THIS IF STATEMENT --> UNDER IF NOT SELECTED SO WE CAN INTERACT WITH UI BUTTONS
         if (selected == false){
             if (Input.GetMouseButtonDown(0))
@@ -53,10 +68,34 @@ public class MainBaseScript : MonoBehaviour
             }
         }
 
-    if (selected){
-        BaseCanvas.SetActive(true);
-    } else {
-        BaseCanvas.SetActive(false);
+        if (selected){
+            BaseCanvas.SetActive(true);
+        } else {
+            BaseCanvas.SetActive(false);
+        }
     }
+
+    public void SetBuildModePowerPlant(){
+        /* Will only build if it meets the requirements
+        Money: 100
+        Power: N/A
+        Metals: 10
+        Tech level: N/A
+        */
+        if (money >= 100 && metals >= 10){
+            PowerPlantBtn.image.color = Color.green;
+            BuildModePowerPlant = true;
+            money -= 100;
+            metals -= 10;
+            power += 25;
+            selected = false;
+        }
+    }
+
+    public void updateStatsUI(){
+        moneyText.text = money.ToString();
+        powerText.text = power.ToString();
+        metalsText.text = metals.ToString();
+        techlvlText.text = techlvl.ToString();
     }
 }
